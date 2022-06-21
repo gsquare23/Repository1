74de88ec702ecad12789523feb534930ba3e4ba5
garvit.c.gupta <garvit.c.gupta@P2CLAP-52GXD63.dir.svc.accenture.com>
2022-06-21 16:26:15 +0530

    First Commit

---
+++ GitHubDemo2/src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.pages;
+
+public class LoginPage {
+
+	public void login() {
+		System.out.println("This is login page");
+	}
+}
---
+++ GitHubDemo2/src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,8 @@
+package com.testcases;
+
+public class LoginTest {
+
+	public void testcase1() {
+		System.out.println("This is login testcase1");
+	}
+}
